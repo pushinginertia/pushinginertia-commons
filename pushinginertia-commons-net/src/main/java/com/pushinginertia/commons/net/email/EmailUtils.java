2923a76d9c5a4dc24df201ec675fa386a483e878
dan <dan.traczynski@gmail.com>
2013-02-21 11:16:54 +0100

    new method to transform a text message to html message

--- pushinginertia-commons-net/src/main/java/com/pushinginertia/commons/net/email/EmailUtils.java
+++ pushinginertia-commons-net/src/main/java/com/pushinginertia/commons/net/email/EmailUtils.java
@@ -1,3 +1,18 @@
+/* Copyright (c) 2011-2013 Pushing Inertia
+ * All rights reserved.  http://pushinginertia.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pushinginertia.commons.net.email;
 
 import org.apache.commons.mail.EmailException;
@@ -12,6 +27,32 @@ import javax.mail.internet.MimeMultipart;
  */
 public class EmailUtils {
 	private static final String UTF_8 = "utf-8";
+
+	/**
+	 * Converts a text message into an HTML message, replacing all newlines with &lt;br&gt; tags and escaping the '&lt;'
+	 * and '&gt;' characters;
+	 * @param s message in text format
+	 * @return message in html format
+	 */
+	public static String textToHtml(final String s) {
+		final StringBuilder sb = new StringBuilder();
+		for (final char c: s.toCharArray()) {
+			switch (c) {
+				case '\n':
+					sb.append("<br/>");
+					break;
+				case '<':
+					sb.append("&lt;");
+					break;
+				case '>':
+					sb.append("&gt;");
+					break;
+				default:
+					sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
 
 	/**
 	 * Populates a newly instantiated {@link MultiPartEmail} with the given arguments.
