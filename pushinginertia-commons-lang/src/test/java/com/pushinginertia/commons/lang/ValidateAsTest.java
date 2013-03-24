3ecc482a5d7a2e9dee8cc1f4e99c97bdd036ea57
dan <dan.traczynski@gmail.com>
2013-03-24 15:36:51 +0100

    new notEqual method

--- pushinginertia-commons-lang/src/test/java/com/pushinginertia/commons/lang/ValidateAsTest.java
+++ pushinginertia-commons-lang/src/test/java/com/pushinginertia/commons/lang/ValidateAsTest.java
@@ -23,6 +23,16 @@ public class ValidateAsTest {
 		ValidateAs.notNull(null, "nullParameter");
 	}
 
+	@Test
+	public void notEqual() {
+		ValidateAs.notEqual("abc", "def", "string1", "string2");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void notEqualFail() {
+		ValidateAs.notEqual("abc", "abc", "string1", "string2");
+	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void positive0() {
 		ValidateAs.positive(0, "zeroParameter");
