8dbffab1230f5929a5543dd926f51af1cb275553
dan <dan.traczynski@gmail.com>
2013-06-07 17:09:30 +0200

    unit test

--- pushinginertia-commons-domain/src/test/java/com/pushinginertia/commons/domain/util/EmailSubscriptionUtilsTest.java
+++ pushinginertia-commons-domain/src/test/java/com/pushinginertia/commons/domain/util/EmailSubscriptionUtilsTest.java
@@ -1,24 +1,41 @@
+/* Copyright (c) 2011-2013 Pushing Inertia
+ * All rights reserved.  http://pushinginertia.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pushinginertia.commons.domain.util;
 
-import junit.framework.TestCase;
 import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class EmailSubscriptionUtilsTest extends TestCase {
+public class EmailSubscriptionUtilsTest {
+	@Test
 	public void testCalculateNextNotificationTimestamp() {
 		final DateTime last = new DateTime(2013, 1, 1, 12, 23, 34, 999); // 2013-01-01 12:23:34
 		final DateTime next = EmailSubscriptionUtils.calculateNextNotificationTimestamp(last, 5);
-		assertEquals(2013, next.getYear());
-		assertEquals(1, next.getMonthOfYear());
-		assertEquals(6, next.getDayOfMonth());
-		assertEquals(12, next.getHourOfDay());
-		assertEquals(23, next.getMinuteOfHour());
-		assertEquals(34, next.getSecondOfMinute());
-		assertEquals(0, next.getMillisOfSecond());
+		Assert.assertEquals(2013, next.getYear());
+		Assert.assertEquals(1, next.getMonthOfYear());
+		Assert.assertEquals(6, next.getDayOfMonth());
+		Assert.assertEquals(12, next.getHourOfDay());
+		Assert.assertEquals(23, next.getMinuteOfHour());
+		Assert.assertEquals(34, next.getSecondOfMinute());
+		Assert.assertEquals(0, next.getMillisOfSecond());
 	}
 
 	public void testCalculateNextNotificationTimestampNull() {
 		final DateTime now = DateTime.now().withMillisOfSecond(0).plusDays(5);
 		final DateTime next = EmailSubscriptionUtils.calculateNextNotificationTimestamp((DateTime)null, 5);
-		assertEquals(now, next);
+		Assert.assertEquals(now, next);
 	}
 }
