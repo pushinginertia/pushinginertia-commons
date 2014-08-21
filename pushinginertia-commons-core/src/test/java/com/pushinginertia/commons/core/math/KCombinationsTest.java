9fee5f4f8e1a38bf63f1739e6d3eff13c9f89ad3
dan <dan.traczynski@gmail.com>
2014-08-21 16:48:37 +0100

    Fixed junit version in KCombinationsTest.

--- pushinginertia-commons-core/src/test/java/com/pushinginertia/commons/core/math/KCombinationsTest.java
+++ pushinginertia-commons-core/src/test/java/com/pushinginertia/commons/core/math/KCombinationsTest.java
@@ -15,14 +15,15 @@
  */
 package com.pushinginertia.commons.core.math;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class KCombinationsTest extends TestCase {
+public class KCombinationsTest {
 	public static class TestPayload implements Combination.Payload<TestPayload> {
 		private Set<Integer> vals;
 
@@ -53,57 +54,61 @@ public class KCombinationsTest extends TestCase {
 		}
 	}
 
+	@Test
 	public void test1() {
 		final List<TestPayload> payloadList = new ArrayList<TestPayload>();
 		payloadList.add(new TestPayload(800, 950));
 		final KCombinations<TestPayload> nk = new KCombinations<TestPayload>(payloadList);
-		assertEquals(1, nk.size());
-		assertEquals(1, nk.rowCount());
-		assertEquals(1, nk.rowSize(0));
+		Assert.assertEquals(1, nk.size());
+		Assert.assertEquals(1, nk.rowCount());
+		Assert.assertEquals(1, nk.rowSize(0));
 		final TestPayload merged = nk.merge(new TestPayload());
-		assertEquals(2, merged.size());
-		assertTrue(merged.containsValue(800));
-		assertTrue(merged.containsValue(950));
+		Assert.assertEquals(2, merged.size());
+		Assert.assertTrue(merged.containsValue(800));
+		Assert.assertTrue(merged.containsValue(950));
 	}
 
+	@Test
 	public void test2() {
 		final List<TestPayload> payloadList = new ArrayList<TestPayload>();
 		payloadList.add(new TestPayload(800, 950));
 		payloadList.add(new TestPayload(850, 1000, 1200));
 		final KCombinations<TestPayload> nk = new KCombinations<TestPayload>(payloadList);
-		assertEquals(3, nk.size());
-		assertEquals(2, nk.rowCount());
-		assertEquals(2, nk.rowSize(0));
-		assertEquals(1, nk.rowSize(1));
+		Assert.assertEquals(3, nk.size());
+		Assert.assertEquals(2, nk.rowCount());
+		Assert.assertEquals(2, nk.rowSize(0));
+		Assert.assertEquals(1, nk.rowSize(1));
 		final TestPayload merged = nk.merge(new TestPayload());
-		assertEquals(5, merged.size());
-		assertTrue(merged.containsValue(800));
-		assertTrue(merged.containsValue(850));
-		assertTrue(merged.containsValue(950));
-		assertTrue(merged.containsValue(1000));
-		assertTrue(merged.containsValue(1200));
+		Assert.assertEquals(5, merged.size());
+		Assert.assertTrue(merged.containsValue(800));
+		Assert.assertTrue(merged.containsValue(850));
+		Assert.assertTrue(merged.containsValue(950));
+		Assert.assertTrue(merged.containsValue(1000));
+		Assert.assertTrue(merged.containsValue(1200));
 	}
 
+	@Test
 	public void test3() {
 		final List<TestPayload> payloadList = new ArrayList<TestPayload>();
 		payloadList.add(new TestPayload(800, 950));
 		payloadList.add(new TestPayload(850, 1000, 1200));
 		payloadList.add(new TestPayload(1200));
 		final KCombinations<TestPayload> nk = new KCombinations<TestPayload>(payloadList);
-		assertEquals(7, nk.size());
-		assertEquals(3, nk.rowCount());
-		assertEquals(3, nk.rowSize(0));
-		assertEquals(3, nk.rowSize(1));
-		assertEquals(1, nk.rowSize(2));
+		Assert.assertEquals(7, nk.size());
+		Assert.assertEquals(3, nk.rowCount());
+		Assert.assertEquals(3, nk.rowSize(0));
+		Assert.assertEquals(3, nk.rowSize(1));
+		Assert.assertEquals(1, nk.rowSize(2));
 		final TestPayload merged = nk.merge(new TestPayload());
-		assertEquals(5, merged.size());
-		assertTrue(merged.containsValue(800));
-		assertTrue(merged.containsValue(850));
-		assertTrue(merged.containsValue(950));
-		assertTrue(merged.containsValue(1000));
-		assertTrue(merged.containsValue(1200));
+		Assert.assertEquals(5, merged.size());
+		Assert.assertTrue(merged.containsValue(800));
+		Assert.assertTrue(merged.containsValue(850));
+		Assert.assertTrue(merged.containsValue(950));
+		Assert.assertTrue(merged.containsValue(1000));
+		Assert.assertTrue(merged.containsValue(1200));
 	}
 
+	@Test
 	public void test4() {
 		final List<TestPayload> payloadList = new ArrayList<TestPayload>();
 		payloadList.add(new TestPayload(800, 950));
@@ -111,22 +116,23 @@ public class KCombinationsTest extends TestCase {
 		payloadList.add(new TestPayload(1200));
 		payloadList.add(new TestPayload(750));
 		final KCombinations<TestPayload> nk = new KCombinations<TestPayload>(payloadList);
-		assertEquals(15, nk.size());
-		assertEquals(4, nk.rowCount());
-		assertEquals(4, nk.rowSize(0));
-		assertEquals(6, nk.rowSize(1));
-		assertEquals(4, nk.rowSize(2));
-		assertEquals(1, nk.rowSize(3));
+		Assert.assertEquals(15, nk.size());
+		Assert.assertEquals(4, nk.rowCount());
+		Assert.assertEquals(4, nk.rowSize(0));
+		Assert.assertEquals(6, nk.rowSize(1));
+		Assert.assertEquals(4, nk.rowSize(2));
+		Assert.assertEquals(1, nk.rowSize(3));
 		final TestPayload merged = nk.merge(new TestPayload());
-		assertEquals(6, merged.size());
-		assertTrue(merged.containsValue(750));
-		assertTrue(merged.containsValue(800));
-		assertTrue(merged.containsValue(850));
-		assertTrue(merged.containsValue(950));
-		assertTrue(merged.containsValue(1000));
-		assertTrue(merged.containsValue(1200));
+		Assert.assertEquals(6, merged.size());
+		Assert.assertTrue(merged.containsValue(750));
+		Assert.assertTrue(merged.containsValue(800));
+		Assert.assertTrue(merged.containsValue(850));
+		Assert.assertTrue(merged.containsValue(950));
+		Assert.assertTrue(merged.containsValue(1000));
+		Assert.assertTrue(merged.containsValue(1200));
 	}
 
+	@Test
 	public void test5() {
 		final List<TestPayload> payloadList = new ArrayList<TestPayload>();
 		payloadList.add(new TestPayload(800, 950));
@@ -135,20 +141,20 @@ public class KCombinationsTest extends TestCase {
 		payloadList.add(new TestPayload(750));
 		payloadList.add(new TestPayload(800, 850));
 		final KCombinations<TestPayload> nk = new KCombinations<TestPayload>(payloadList);
-		assertEquals(31, nk.size());
-		assertEquals(5, nk.rowCount());
-		assertEquals(5, nk.rowSize(0));
-		assertEquals(10, nk.rowSize(1));
-		assertEquals(10, nk.rowSize(2));
-		assertEquals(5, nk.rowSize(3));
-		assertEquals(1, nk.rowSize(4));
+		Assert.assertEquals(31, nk.size());
+		Assert.assertEquals(5, nk.rowCount());
+		Assert.assertEquals(5, nk.rowSize(0));
+		Assert.assertEquals(10, nk.rowSize(1));
+		Assert.assertEquals(10, nk.rowSize(2));
+		Assert.assertEquals(5, nk.rowSize(3));
+		Assert.assertEquals(1, nk.rowSize(4));
 		final TestPayload merged = nk.merge(new TestPayload());
-		assertEquals(6, merged.size());
-		assertTrue(merged.containsValue(750));
-		assertTrue(merged.containsValue(800));
-		assertTrue(merged.containsValue(850));
-		assertTrue(merged.containsValue(950));
-		assertTrue(merged.containsValue(1000));
-		assertTrue(merged.containsValue(1200));
+		Assert.assertEquals(6, merged.size());
+		Assert.assertTrue(merged.containsValue(750));
+		Assert.assertTrue(merged.containsValue(800));
+		Assert.assertTrue(merged.containsValue(850));
+		Assert.assertTrue(merged.containsValue(950));
+		Assert.assertTrue(merged.containsValue(1000));
+		Assert.assertTrue(merged.containsValue(1200));
 	}
 }
